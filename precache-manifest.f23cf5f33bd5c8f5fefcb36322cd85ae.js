b32e200ad240b68824496194bc5b9a3d00a9a3c9
Ilya <senior.smikow@yandex.ru>
2020-10-25 11:56:54 +0300

    Updates

---
+++ precache-manifest.f23cf5f33bd5c8f5fefcb36322cd85ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238079a9fd74cb34ac5d95a4d59eed09",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "743f4b9265db1e3172ae",
+    "url": "/react_project/static/css/main.5c099f7f.chunk.css"
+  },
+  {
+    "revision": "b63d8b8ecc31cc79603c",
+    "url": "/react_project/static/js/2.c5e73688.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/react_project/static/js/2.c5e73688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743f4b9265db1e3172ae",
+    "url": "/react_project/static/js/main.57c3701d.chunk.js"
+  },
+  {
+    "revision": "7f7b39d0d68f58f8bb33",
+    "url": "/react_project/static/js/runtime-main.e056913e.js"
+  },
+  {
+    "revision": "d9f433b042eda9ab9f56fda249e11bec",
+    "url": "/react_project/static/media/img_568657.d9f433b0.png"
+  }
+]);
